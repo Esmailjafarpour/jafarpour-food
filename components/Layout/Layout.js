692da7994af6d889a3becbdebd544cca5f8715a9
nader <esmailjafarpour29@gmail.com>
2023-07-30 14:01:19 +0330

    create layout

---
+++ components/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from "./Layout.module.css";
+import Link from "next/link";
+
+const Layout = ({children}) => {
+     return (
+          <>
+            <header className={styles.header}>
+
+               <div className={styles.left}>
+                    <Link href="/">jafarpourfood</Link>
+               </div>
+
+               <div className={styles.right}>
+                    <Link href="/menu">Menu</Link>
+                    <Link href="/categories">Categories</Link>
+               </div>
+
+            </header>  
+
+            <div className={styles.container}>
+               {children}
+            </div> 
+
+            <footer className={styles.footer}>
+               <a href="https://botostart.ir" target="_blank" rel="noreferrer">Jafarpour Food</a>
+               Next.js course | BotoFood Project &copy:
+            </footer>
+          </>
+     );
+}
+
+export default Layout;
